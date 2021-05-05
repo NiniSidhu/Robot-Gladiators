485ba33fe242a2d4fef5531aa31b03a678c2503f
NayanSidhu <nsidh068@uottawa.ca>
2021-05-04 23:49:21 -0400

    add enemy combats

--- assets/game.js
+++ assets/game.js
@@ -1,39 +1,4 @@
-/* Window.Alert function provides the user with an alert. The user cannot continue without acknowledging the alert!
- Window refers to the browser iteself in the above and hence will alaways be present! 
- Alert is refered to as a Function of JS. One of many!
- Content is is always placed in quites in JS; called assing an argument into a function 
- ; a semicolon closes the alert function code. 
-// window.alert("This is an alert! JavaScript is running!"); 
 
-var playerName = window.prompt("What is your robot's name?"); // Promts the user for input
-//"var" aka variable is used to store the value that the user has inputed under variable name playerName in this case. 
-// JS cannot understand hyphens... so use Camel Casing!
-// Console logs show in Chrome Dev Tools; used mainly for coding in JS 
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// This console log will do Math 
-console.log(10+10);
-//Will shoot out a variable along with a string and the + sign is needed for different arguments 
-console.log("Our robot's name is" + playerName);
-
-// Data Types 
-
- //var stringDataType = "String data type; aka text "
- //var numberIntegerDataType = 10;
- //var numberFloatDataType = 10.4;
-// var booleanDataType = true;
-
-
-
-// This creates a function named "Fight"
-function fight(){
-    window.alert("The fight has begun!");
-}
-//fight(); // This calls the function fight!
-
-// "Function" is the function with name fight 
-// () are the function paranteses 
-// When declaring a function we use the keyword "Function" */
 
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
@@ -44,36 +9,13 @@ var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"]; // Defined 3 enemy 
 var enemyHealth = 50;
 var enemyAttack = 12; 
 
+/*console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);*/
 
 
-
-/*console.log(playerName, playerAttack, playerHealth); // We could log mutiple items at once as well!
-console.log(enemyNames); //Retrieves all the names of the enemies
-console.log(enemyNames[0]); //Retrieves the 0th name in the array defined above
-console.log(enemyNames[1]);
-console.log(enemyNames[2]);
-console.log(enemyNames.length); // tells us the length of the Array!
-//console.log(enemyNames[enemyNames.length-1]); //tells us the last element in the array. 
-/* the for loop is used to display the contents of the array without individully calling them.     
-the following is the convention for the for loop: 
-
-for([initial expression]; [condition]; [increment expression]){
-    statement
-} 
-
-for (var i =0; i <3; i++){
-        console.log("apple", i); shows the iteration of the array in the console 
-    } */
-
-    
-for (var i = 0; i < enemyNames.length; i++){
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
-
-
-var fight = function(){
+var fight = function(enemyName){
     
     //Alert the player that ehy are starting the round.
     window.alert("Welcome to Robot Gladiators!");
@@ -127,8 +69,68 @@ var fight = function(){
             }
 
     }   
+for (var i = 0; i < enemyNames.length; i++){
+        console.log(enemyNames[i]);
+}      
+    
+
+ 
 
 
 
 
 
+    /* Window.Alert function provides the user with an alert. The user cannot continue without acknowledging the alert!
+ Window refers to the browser iteself in the above and hence will alaways be present! 
+ Alert is refered to as a Function of JS. One of many!
+ Content is is always placed in quites in JS; called assing an argument into a function 
+ ; a semicolon closes the alert function code. 
+// window.alert("This is an alert! JavaScript is running!"); 
+
+var playerName = window.prompt("What is your robot's name?"); // Promts the user for input
+//"var" aka variable is used to store the value that the user has inputed under variable name playerName in this case. 
+// JS cannot understand hyphens... so use Camel Casing!
+// Console logs show in Chrome Dev Tools; used mainly for coding in JS 
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+// This console log will do Math 
+console.log(10+10);
+//Will shoot out a variable along with a string and the + sign is needed for different arguments 
+console.log("Our robot's name is" + playerName);
+
+// Data Types 
+
+ //var stringDataType = "String data type; aka text "
+ //var numberIntegerDataType = 10;
+ //var numberFloatDataType = 10.4;
+// var booleanDataType = true;
+
+
+
+// This creates a function named "Fight"
+function fight(){
+    window.alert("The fight has begun!");
+}
+//fight(); // This calls the function fight!
+
+// "Function" is the function with name fight 
+// () are the function paranteses 
+// When declaring a function we use the keyword "Function" */
+
+/*console.log(playerName, playerAttack, playerHealth); // We could log mutiple items at once as well!
+console.log(enemyNames); //Retrieves all the names of the enemies
+console.log(enemyNames[0]); //Retrieves the 0th name in the array defined above
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length); // tells us the length of the Array!
+//console.log(enemyNames[enemyNames.length-1]); //tells us the last element in the array. 
+/* the for loop is used to display the contents of the array without individully calling them.     
+the following is the convention for the for loop: 
+
+for([initial expression]; [condition]; [increment expression]){
+    statement
+} 
+
+for (var i =0; i <3; i++){
+        console.log("apple", i); shows the iteration of the array in the console 
+    } */
