10d58cb8b9ba335e7bcfb046be8d6f478f0f3a89
NayanSidhu <nsidh068@uottawa.ca>
2021-05-07 23:29:50 -0400

    multiple rounds complete

--- assets/game.js
+++ assets/game.js
@@ -57,9 +57,19 @@ var fight = function(enemyNames){   // calling fight as a function and variable 
 };
 
 for(var i = 0 ; i < enemyNames.length; i++){
-    //debugger;
-    enemyHealth = 50;
-    fight(enemyNames[i]);
+    if (playerHealth > 0){
+        window.alert("Welcome to Robot Gladiators! Round" + (i + 1));
+
+        var pickedEnemyName = enemyNames [i];
+        enemyHealth = 50;
+        fight(enemyNames[i]);
+    }
+    else{
+        window.alert("You have lost your robot in battle! Game Khalaas");
+        break;
+    }
+    
+    
 }
 
 
